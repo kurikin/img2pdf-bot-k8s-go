417cbe93e3d850ccdb5cc4f26dcaddc54904f44f
kurikin <riku.code0309@gmail.com>
2023-03-25 19:19:34 +0900

    Update main.go

--- go.mod
+++ go.mod
@@ -3,14 +3,23 @@ module github.com/kurikin/linebot-k8s-go
 go 1.19
 
 require (
+	cloud.google.com/go/firestore v1.9.0
+	cloud.google.com/go/storage v1.30.1
+	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible
+	google.golang.org/api v0.114.0
+)
+
+require (
 	cloud.google.com/go v0.110.0 // indirect
 	cloud.google.com/go/compute v1.19.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/firestore v1.9.0 // indirect
+	cloud.google.com/go/iam v0.13.0 // indirect
 	cloud.google.com/go/longrunning v0.4.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
@@ -21,7 +30,6 @@ require (
 	golang.org/x/text v0.8.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/api v0.114.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230323212658-478b75c54725 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,206 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/storage"
+	"github.com/jung-kurt/gofpdf"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"google.golang.org/api/option"
 )
 
+type Image struct {
+	UserID    string
+	ImageURL  string
+	FileName  string
+	CreatedAt time.Time
+}
+
+type UserState struct {
+	state     string
+	imageData *Image
+}
+
+var userStates = make(map[string]*UserState)
+
+func isFileNameValid(fileName string) bool {
+	trimmed := strings.TrimSpace(fileName)
+	if trimmed == "" {
+		return false
+	}
+	if strings.Contains(trimmed, "pdf") {
+		return false
+	}
+	return true
+}
+
+func createFirestoreClient(ctx context.Context) *firestore.Client {
+	saPath := "path/to/your-service-account-key.json"
+
+	client, err := firestore.NewClient(ctx, "your-project-id", option.WithCredentialsFile(saPath))
+	if err != nil {
+		log.Fatalf("Failed to create Firestore client: %v", err)
+	}
+
+	return client
+}
+
+func SaveImageToFirestore(ctx context.Context, client *firestore.Client, img *Image) {
+	docRef := client.Collection("images").Doc(img.UserID)
+	_, err := docRef.Set(ctx, img)
+	if err != nil {
+		log.Printf("Failed to save image to Firestore: %v", err)
+	}
+}
+
+func createLineBotClient() *linebot.Client {
+	channelSecret := "your_channel_secret"
+	channelAccessToken := "your_channel_access_token"
+
+	bot, err := linebot.New(channelSecret, channelAccessToken)
+	if err != nil {
+		log.Fatalf("Failed to create LINE bot client: %v", err)
+	}
+
+	return bot
+}
+
+func uploadToCloudStorage(ctx context.Context, bucketName, objectName, localFile string) error {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	f, err := os.Open(localFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	wc := client.Bucket(bucketName).Object(objectName).NewWriter(ctx)
+	if _, err = io.Copy(wc, f); err != nil {
+		return err
+	}
+	if err := wc.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func convertImageToPDF(imagePath, pdfPath string) error {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+
+	imageOptions := gofpdf.ImageOptions{
+		ImageType: "JPG",
+	}
+
+	width, _ := pdf.GetPageSize()
+	pdf.ImageOptions(imagePath, 0, 0, width, 0, false, imageOptions, 0, "")
+
+	return pdf.OutputFileAndClose(pdfPath)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, GKE!")
+	ctx := context.Background()
+	firestoreClient := createFirestoreClient(ctx)
+	lineBotClient := createLineBotClient()
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		events, err := lineBotClient.ParseRequest(r)
+		if err != nil {
+			log.Printf("Failed to parse request: %v", err)
+			return
+		}
+
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				userID := event.Source.UserID
+				state := userStates[userID]
+
+				switch message := event.Message.(type) {
+				case *linebot.ImageMessage:
+					if state != nil && state.state == "waiting_for_filename" {
+						continue
+					}
+					imageURL := message.OriginalContentURL
+
+					img := &Image{
+						UserID:    userID,
+						ImageURL:  imageURL,
+						FileName:  "", // Set later when the user provides the filename
+						CreatedAt: time.Now(),
+					}
+
+					userStates[userID] = &UserState{
+						state:     "waiting_for_filename",
+						imageData: img,
+					}
+
+					_, err = lineBotClient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("画像を受け取りました。ファイル名を入力してください。")).Do()
+					if err != nil {
+						log.Printf("Failed to reply message: %v", err)
+					}
+				case *linebot.TextMessage:
+					if state != nil && state.state == "waiting_for_filename" {
+						fileName := message.Text
+
+						if !isFileNameValid(fileName) {
+							_, err = lineBotClient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("無効なファイル名です。もう一度入力してください。")).Do()
+							if err != nil {
+								log.Printf("Failed to reply message: %v", err)
+							}
+							continue
+
+						}
+
+						// Set the filename for the image
+						state.imageData.FileName = fileName
+
+						// Convert image to PDF
+						pdfPath := userID + ".pdf"
+						err = convertImageToPDF(state.imageData.ImageURL, pdfPath)
+						if err != nil {
+							log.Printf("Failed to convert image to PDF: %v", err)
+							continue
+						}
+
+						// Upload PDF to Google Cloud Storage
+						bucketName := "your-bucket-name"
+						objectName := userID + "/" + pdfPath
+						err = uploadToCloudStorage(ctx, bucketName, objectName, pdfPath)
+						if err != nil {
+							log.Printf("Failed to upload PDF to Cloud Storage: %v", err)
+							continue
+						}
+
+						// Generate download URL for the uploaded PDF
+						downloadURL := "https://storage.googleapis.com/" + bucketName + "/" + objectName
+
+						// Save the image data to Firestore
+						SaveImageToFirestore(ctx, firestoreClient, state.imageData)
+
+						// Reset the user state
+						userStates[userID] = nil
+
+						_, err = lineBotClient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("PDFに変換しました。"+downloadURL)).Do()
+						if err != nil {
+							log.Printf("Failed to reply message: %v", err)
+						}
+					}
+				}
+			}
+		}
 	})
 
-	port := "8080"
-	fmt.Printf("Starting server on port %s\n", port)
-	http.ListenAndServe(":"+port, nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
